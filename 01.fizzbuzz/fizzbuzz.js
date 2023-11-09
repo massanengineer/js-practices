bf682ed9abbf4e384096b05819b1d0cc7177a7e1
massanengineer <matsuda4645@gmail.com>
2023-11-10 02:04:38 +0900

    Prettier修正

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,10 +1,9 @@
-for (let num1 = 1; num1 <= 20; num1++){
-  if(num1 % 15 == 0 ){
-    console.log('FizzBuzz');
+for (let num1 = 1; num1 <= 20; num1++) {
+  if (num1 % 15 == 0) {
+    console.log("FizzBuzz");
   } else if (num1 % 5 == 0) {
-    console.log('Buzz')
+    console.log("Buzz");
   } else if (num1 % 3 == 0) {
-    console.log('Fizz')
-  } else
-  console.log(num1);
+    console.log("Fizz");
+  } else console.log(num1);
 }
